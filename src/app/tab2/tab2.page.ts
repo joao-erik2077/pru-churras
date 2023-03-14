b74461cd7504c2b73f40865e2d1b0bb29d4e8134
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2023-03-14 19:01:57 -0300

    page 2 and remove explorer container

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,73 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  roundValue = 1;
+  points = {
+    teamOne: 0,
+    teamTwo: 0
+  };
+  wins = {
+    teamOne: 0,
+    teamTwo: 0
+  };
+
   constructor() {}
 
+  changeRoundValue(value: number) {
+    this.roundValue = value;
+  }
+
+  addPointTo(team: number) {
+    if (team === 1) {
+      this.points.teamOne++;
+      this.checkRoundWin()
+    } else if (team === 2) {
+      this.points.teamTwo++;
+      this.checkRoundWin()
+    }
+  }
+
+  checkRoundWin() {
+    if (this.points.teamOne >= 12) {
+      this.points = {
+        teamOne: 0,
+        teamTwo: 0
+      };
+      this.wins.teamOne++;
+    } else if (this.points.teamTwo >= 12) {
+      this.points = {
+        teamOne: 0,
+        teamTwo: 0
+      };
+      this.wins.teamTwo++;
+    }
+  }
+
+  removePointFrom(team: number) {
+    if (team === 1) {
+      this.points.teamOne--;
+    } else if (team === 2) {
+      this.points.teamTwo--;
+    }
+  }
+
+  addWinTo(team: number) {
+    if (team === 1) {
+      this.wins.teamOne++;
+    } else if (team === 2) {
+      this.wins.teamTwo++;
+    }
+  }
+
+  clear() {
+    this.roundValue = 1;
+    this.points = {
+      teamOne: 0,
+      teamTwo: 0
+    };
+    this.wins = {
+      teamOne: 0,
+      teamTwo: 0
+    };
+  }
 }
