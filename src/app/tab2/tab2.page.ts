3f3682867678e0efea1c7ef29709318732ae8747
joao-erik2077 <joaoerik2074@gmail.com>
2023-03-16 08:43:50 -0300

    style tab2
    "E não vos conformeis a este mundo, mas transformai-vos pela renovação da vossa mente."
    Paulo de Tarso

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -8,6 +8,14 @@ import { ToastController } from '@ionic/angular';
 })
 export class Tab2Page {
 
+  trucoAudio = new Audio('../../assets/audio/truco.mp3');
+
+  btFill1 = 'outline';
+  btFill3 = 'solid';
+  btFill6 = 'solid';
+  btFill9 = 'solid';
+  btFill12 = 'solid';
+
   roundValue = 1;
   points = {
     teamOne: 0,
@@ -22,6 +30,33 @@ export class Tab2Page {
 
   changeRoundValue(value: number) {
     this.roundValue = value;
+    this.resetBtnFills();
+
+    switch (value) {
+      case 1:
+        this.btFill1 = 'outline';
+        break;
+      case 3:
+        this.btFill3 = 'outline';
+        break;
+      case 6:
+        this.btFill6 = 'outline';
+        break;
+      case 9:
+        this.btFill9 = 'outline';
+        break;
+      case 12:
+        this.btFill12 = 'outline';
+        break;
+    }
+  }
+
+  resetBtnFills() {
+    this.btFill1 = 'solid';
+    this.btFill3 = 'solid';
+    this.btFill6 = 'solid';
+    this.btFill9 = 'solid';
+    this.btFill12 = 'solid';
   }
 
   addPointTo(team: number) {
@@ -32,6 +67,8 @@ export class Tab2Page {
       this.points.teamTwo += this.roundValue;
       this.checkRoundWin()
     }
+    this.resetBtnFills();
+    this.btFill1 = 'outline';
     this.roundValue = 1;
   }
 
@@ -54,9 +91,9 @@ export class Tab2Page {
   }
 
   removePointFrom(team: number) {
-    if (team === 1) {
+    if (team === 1 && this.points.teamOne > 0) {
       this.points.teamOne--;
-    } else if (team === 2) {
+    } else if (team === 2 && this.points.teamTwo > 0) {
       this.points.teamTwo--;
     }
   }
@@ -90,5 +127,9 @@ export class Tab2Page {
     });
 
     await toast.present();
+  }
+
+  trucar() {
+    this.trucoAudio.play();
   }
 }
