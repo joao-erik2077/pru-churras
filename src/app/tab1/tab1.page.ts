98e8a96fb97c4edea004fe6c235517d4a83e5455
joao-erik2077 <joaoerik2074@gmail.com>
2023-03-23 08:57:18 -0300

    animation in tab1 and tab3 diceroll

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  animations: [
+    trigger('rotatedState', [
+      state('default', style({ transform: 'rotateY(o)'})),
+      state('rotated', style({ transform: 'rotateY(-360deg)'})),
+      transition('rotated => default', animate('2000ms ease-out')),
+      transition('default => rotated', animate('2000ms ease-in'))
+    ])
+  ]
 })
 export class Tab1Page {
 
@@ -12,16 +21,21 @@ export class Tab1Page {
   logo = 'assets/logo.png';
   image = this.logo;
   info = 'Clique no botão para jogar!';
+  state = 'default';
 
   constructor() {}
 
   jogarMoeda() {
-    if (Math.random() < 0.5) {
-      this.image = this.cara;
-      this.info = 'Cara!';
-    } else {
-      this.image = this.coroa;
-      this.info = 'Coroa!';
-    }
+    this.state = this.state === 'default' ? 'rotated' : 'default';
+
+    setTimeout(() => {
+      if (Math.random() < 0.5) {
+        this.image = this.cara;
+        this.info = 'Cara!';
+      } else {
+        this.image = this.coroa;
+        this.info = 'Coroa!';
+      }
+    }, 2000);
   }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,42 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  diceValue: number = 6;
+  result: number = 0;
+  canRoll: boolean = true;
+  previousResults: any = [];
+  dice = 'assets/dice.png';
+  diceRoll = 'assets/diceroll.gif';
+  actualImage = this.dice;
 
+  constructor(private alertController: AlertController) {}
+
+  rollDice() {
+    if (this.canRoll) {
+      this.canRoll = false;
+      this.actualImage = this.diceRoll;
+      setTimeout(() => {
+        this.result = Math.ceil((Math.random() * this.diceValue));
+        this.previousResults.push(
+          {
+            result: this.result,
+            diceValue: this.diceValue
+          }
+        );
+        this.actualImage = this.dice;
+        this.showResults();
+        this.canRoll = true;
+      }, 1500);
+    }
+  }
+
+  async showResults() {
+    const alert = await this.alertController.create({
+      header: 'Resultado',
+      message: `Resultado: ${this.result}`,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
 }
